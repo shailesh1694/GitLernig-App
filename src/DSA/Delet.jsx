20c4d43609a71f5e4102a19db90083049f1125a1
shailesh1694 <morishailesh982@gmail.com>
2023-04-23 11:04:06 +0530

    merge

--- src/DSA/Delet.jsx
+++ src/DSA/Delet.jsx
@@ -4,7 +4,7 @@ function Delet() {
   return (
     <>
     <div>
-      Jay Hind,Jay BharatMataki,Jay Shiva,jay
+      Main 
     </div>
       <h1>Shailesh Mori Git hub Testing Account</h1>
     </>
