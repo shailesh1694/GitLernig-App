f6529dc3ef4aee8cece6fcea8faca1f8be152e7a
shailesh1694 <morishailesh982@gmail.com>
2023-04-23 10:43:29 +0530

    change in

--- src/DSA/Delet.jsx
+++ src/DSA/Delet.jsx
@@ -2,9 +2,12 @@ import React from 'react'
 
 function Delet() {
   return (
+    <>
     <div>
       Jay Hind,Jay BharatMataki,Jay Shiva
     </div>
+      <h1>Shailesh Mori Git hub Testing Account</h1>
+    </>
   )
 }
 
