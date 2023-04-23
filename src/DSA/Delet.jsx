8ccfc86e784ca6cd3e7e14a617fd23c80c6224fb
shailesh1694 <morishailesh982@gmail.com>
2023-04-23 09:07:32 +0530

    git test

--- src/DSA/Delet.jsx
+++ src/DSA/Delet.jsx
@@ -2,12 +2,7 @@ import React from 'react'
 
 function Delet() {
   return (
-    <>
-    <div>
-      {"shailesh"}
-      <h1>Git Hub Testing File to Done With It</h1>
-    </div>
-  </>
+    <h1>MittalMOri</h1>
   )
 }
 
